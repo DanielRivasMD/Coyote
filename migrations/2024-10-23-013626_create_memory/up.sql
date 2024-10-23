bf0f8d0a4b59aaa886cd0acbb4efbb37576adb64
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-22 20:06:10 -0600

    deprecate manually generated sql database

--- migrations/2024-10-23-013626_create_memory/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE memory (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  token TEXT,
-  kind TEXT,
-  score INTEGER,
-  stability INTEGER,
-  retrievability INTEGER,
-  difficulty INTEGER
-);
