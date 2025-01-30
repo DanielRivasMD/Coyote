6513a1bef31c779ca247b9c508d21901f4d3b33e
Daniel Rivas <danielrivasmd@gmail.com>
2025-01-29 20:11:20 -0600

    update logic main coyote

--- src/bin/coyote.rs
+++ src/bin/coyote.rs
@@ -11,12 +11,24 @@ use std::io;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// crate utilities
+use crate::custom::cards::Flashcard;
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
+fn main() -> anyResult<()> {
+  let mut flashcard = Flashcard::new(1);
 
+  flashcard.id = 7;
 
+  // // example usage: simulate user responses with different qualities
+  // let qualities = vec![5, 4, 3, 2, 5, 5];
 
+  loop {
+    let mut answer = String::new();
+    io::stdin().read_line(&mut answer).expect("Failed to read");
 
+		let answer: u32 = match answer.trim().parse() {
 			Ok(num) => num,
 			Err(_) => {
 				println!("Please type a number!");
@@ -24,6 +36,27 @@ use std::io;
 			},
 		};
 
+      println!("You answered: {}", answer);
+      println!("{:?}", flashcard);
+      println!("{}", flashcard.id == answer);
+
+      if flashcard.id == answer {
+        if flashcard.quality < 5 {
+          flashcard.quality += 1;
+          break;
+        } else {
+          if flashcard.quality > 0 {
+            flashcard.quality -= 1;
+          }
+        }
+      }
+
+    flashcard.update();
+    println!(
+      "Repetitions: {}, Interval: {} days, Ease Factor: {:.2}",
+      flashcard.repetitions, flashcard.interval, flashcard.ease_factor
+    );
+  }
 
 	Ok(())
 }
