c228debc68de9cf2295d92360eea7a0d6d5f6e5a
Daniel Rivas <danielrivasmd@gmail.com>
2025-01-30 21:18:02 -0600

    patch casting string cards custom

--- src/custom/cards.rs
+++ src/custom/cards.rs
@@ -23,45 +23,44 @@ pub struct Card {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-impl Flashcard {
+impl Card {
   pub fn new() -> Self {
-    Flashcard {
-      id: String::new(),
-      quality: 0,       // initialize at zero
-      ease_factor: 2.5, // default ease factor
-      interval: 1,      // default interval
-      repetitions: 0,   // no repetitions initially
+    Card {
+      word: String::new(),
+      quality: "0".to_string(),                      // initialize at zero
+      difficulty: "2.5".to_string(),                 // default ease factor
+      interval: "1".to_string(),                     // default interval
+      repetitions: "0".to_string(),                  // no repetitions initially
     }
   }
 
   pub fn update(&mut self) {
-    // ensure quality is in the range [0, 5]
-    // let quality = quality.min(5).max(0);
 
-    if self.quality >= 3 {
-      if self.repetitions == 0 {
-        self.interval = 1;
-      } else if self.repetitions == 1 {
-        self.interval = 6;
+    // quality is locked between 0 - 5
+    if self.quality.parse::<u32>().unwrap() >= 3 {
+      if self.repetitions.parse::<u32>().unwrap() == 0 {
+        self.interval = "1".to_string();
+      } else if self.repetitions.parse::<u32>().unwrap() == 1 {
+        self.interval = "6".to_string();
       } else {
-        self.interval = (self.interval as f64 * self.ease_factor).round() as u32;
+        self.interval = ((self.interval.parse::<f64>().unwrap() * self.difficulty.parse::<f64>().unwrap()).round() as u32).to_string();
       }
-      self.repetitions += 1;
+      self.repetitions = (self.repetitions.parse::<u32>().unwrap() + 1).to_string();
     } else {
-      self.repetitions = 0;
-      self.interval = 1;
+      self.repetitions = "0".to_string();
+      self.interval = "1".to_string();
     }
 
-    // update ease factor
-    self.ease_factor += 0.1 - (5 - self.quality) as f64 * (0.08 + (5 - self.quality) as f64 * 0.02);
-    if self.ease_factor < 1.3 {
-      self.ease_factor = 1.3;
+    // update difficulty
+    self.difficulty = (self.difficulty.parse::<f64>().unwrap() + 0.1 - (5. - self.quality.parse::<f64>().unwrap()) * (0.08 + (5. - self.quality.parse::<f64>().unwrap()) * 0.02)).to_string();
+    if self.difficulty.parse::<f64>().unwrap() < 1.3 {
+      self.difficulty = "1.3".to_string();
     }
   }
 
-  fn next_review_in_days(&self) -> u32 {
-    self.interval
-  }
+  // fn next_review_in_days(&self) -> u32 {
+  //   self.interval
+  // }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
