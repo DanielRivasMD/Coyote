eefc0a3d4362fd168ae4508018fe3d606ec60ed6
Daniel Rivas <danielrivasmd@gmail.com>
2025-01-30 21:15:12 -0600

    silence cli test main coyote

--- src/bin/coyote.rs
+++ src/bin/coyote.rs
@@ -7,49 +7,50 @@ use coyote::*;
 
 // standard libraries
 use anyhow::Result as anyResult;
-use std::io;
-use unicode_normalization::UnicodeNormalization;
+// use std::io;
+// use unicode_normalization::UnicodeNormalization;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // crate utilities
-use crate::custom::cards::Flashcard;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 fn main() -> anyResult<()> {
-  let mut flashcard = Flashcard::new();
-  // hardcoded for debugging
-  flashcard.id = "a".nfc().collect::<String>();
 
-  loop {
-    let mut answer = String::new();
-    io::stdin().read_line(&mut answer).expect("Failed to read");
 
-		answer = answer.trim().nfc().collect::<String>();
-    println!("You answered: {}", answer);
+  // let mut flashcard = Card::new();
+  // // hardcoded for debugging
+  // flashcard.word = "a".nfc().collect::<String>();
 
-    if flashcard.id == answer {
-      if flashcard.quality < 5 {
-        flashcard.quality += 1;
-      }
-    } else {
-      println!("wrong!");
-      if flashcard.quality > 0 {
-        flashcard.quality -= 1;
-      }
-    }
+  // loop {
+  //   let mut answer = String::new();
+  //   io::stdin().read_line(&mut answer).expect("Failed to read");
 
-    flashcard.update();
-    println!(
-      "Quality: {}, Repetitions: {}, Interval: {} days, Ease Factor: {:.2}",
-      flashcard.quality, flashcard.repetitions, flashcard.interval, flashcard.ease_factor
-    );
+  //   answer = answer.trim().nfc().collect::<String>();
+  //   println!("You answered: {}", answer);
 
-    if flashcard.quality == 5 {
-      break
-    }
-  }
+  //   if flashcard.word == answer {
+  //     if flashcard.quality < 5 {
+  //       flashcard.quality += 1;
+  //     }
+  //   } else {
+  //     println!("wrong!");
+  //     if flashcard.quality > 0 {
+  //       flashcard.quality -= 1;
+  //     }
+  //   }
+
+  //   flashcard.update();
+  //   println!(
+  //     "Quality: {}, Repetitions: {}, Interval: {} days, Ease Factor: {:.2}",
+  //     flashcard.quality, flashcard.reps, flashcard.interval, flashcard.difficulty
+  //   );
+
+  //   if flashcard.quality == 5 {
+  //     break
+  //   }
+  // }
 
 	Ok(())
 }
