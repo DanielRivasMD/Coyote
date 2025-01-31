3914728026010e69cc7869c9a0c41b794ce3be04
Daniel Rivas <danielrivasmd@gmail.com>
2025-01-30 21:15:30 -0600

    add sql connection main coyote

--- src/bin/coyote.rs
+++ src/bin/coyote.rs
@@ -7,17 +7,37 @@ use coyote::*;
 
 // standard libraries
 use anyhow::Result as anyResult;
+use clap::Parser;
+use std::fs::File;
+
 // use std::io;
 // use unicode_normalization::UnicodeNormalization;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // crate utilities
+use crate::custom::cards::Card;
+use crate::utils::help::*;
+use crate::utils::sql::*;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 fn main() -> anyResult<()> {
+  // collect command line arguments
+  let params = Cli::parse();
 
+  // get path
+  let file_path = params.input;
+  let _file = File::open(file_path)?;
+
+  // open database connection
+  let mut conn = establish_db_connection()?;
+
+  let mut card = Card::new();
+  card.word = "Hallo".to_string();
+
+  // insert to database
+  insert_insertable_struct(card, &mut conn)?;
 
   // let mut flashcard = Card::new();
   // // hardcoded for debugging
