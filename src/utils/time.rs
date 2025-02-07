347e957a9eeb036d2c93ac15a378bd0f99df1cd4
Daniel Rivas <danielrivasmd@gmail.com>
2025-02-06 21:18:36 -0600

    patch time, current & delta date

--- src/utils/time.rs
+++ src/utils/time.rs
@@ -5,7 +5,7 @@ use anyhow::{
 };
 use chrono::{
   Duration,
-  NaiveDateTime,
+  NaiveDate,
   Utc,
 };
 
@@ -24,7 +24,7 @@ use crate::DATE_FORMAT;
 // get current date
 pub fn current_date() -> String {
   let now = Utc::now().naive_utc();
-  now.format("%Y-%m-%d").to_string()
+  now.format(DATE_FORMAT).to_string()
 }
 
 // add delta date
@@ -33,7 +33,7 @@ pub fn delta_date(
   days: String,
 ) -> anyResult<String> {
   let days = days.parse::<i64>().unwrap();
-  let delta = NaiveDateTime::parse_from_str(&date_str, DATE_FORMAT)?
+  let delta = NaiveDate::parse_from_str(&date_str, "%Y-%m-%d")?
     .checked_add_signed(Duration::days(days))
     .context(CoyoteError::DateParsing)?
     .to_string();
