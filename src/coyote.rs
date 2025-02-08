bd9a87191163af0e4d215d6bb6f930b832af7aea
Daniel Rivas <danielrivasmd@gmail.com>
2025-02-08 11:55:37 -0600

    add train message arrays

--- src/coyote.rs
+++ src/coyote.rs
@@ -20,6 +20,9 @@ pub mod utils;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// TODO: consider changing to static
 const DATE_FORMAT: &str = "%Y-%m-%d";
+const TRAIN_SUCCESS: [&str; 3] = ["Suberb!", "Outstanding!", "Remarkable!"];
+const TRAIN_FAILURE: [&str; 3] = ["Keep it up!", "Do not give up just yet!", "Next time it will be better!"];
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
