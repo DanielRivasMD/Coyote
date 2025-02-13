c67f93cb22c700755381339d6ea17eb0561506ad
Daniel Rivas <danielrivasmd@gmail.com>
2025-02-13 13:39:31 -0600

    implement tryfrom level enum

--- src/custom/level.rs
+++ src/custom/level.rs
@@ -30,3 +30,20 @@ impl fmt::Display for Level {
   }
 }
 
+impl TryFrom<&str> for Level {
+  type Error = String;
+
+  fn try_from(value: &str) -> Result<Self, Self::Error> {
+    match value {
+      "A1" => Ok(Level::A1),
+      "A2" => Ok(Level::A2),
+      "B1" => Ok(Level::B1),
+      "B2" => Ok(Level::B2),
+      "C1" => Ok(Level::C1),
+      "C2" => Ok(Level::C2),
+      _ => Err(format!("Invalid level: {}", value)),
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
