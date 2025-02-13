c62f71796dfa30d4cb0a1f55989881c4ba6e4cbc
Daniel Rivas <danielrivasmd@gmail.com>
2025-02-13 11:07:19 -0600

    refactor training cmd

--- src/cmds/train.rs
+++ src/cmds/train.rs
@@ -1,16 +1,25 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // standard libraries
-use anyhow::Result as anyResult;
+use anyhow::{Context, Result as anyResult};
+use colored::*;
+use crossterm::{
+  event::{self, Event, KeyCode},
+  terminal::{self},
+};
+use diesel::SqliteConnection;
+use rand::{Rng, rng, seq::SliceRandom};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // error handler
+use crate::utils::error::CoyoteError;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // crate utilities
-use crate::utils::{cli::train_cli, sql::set_conn_db};
+use crate::{TRAIN_FAILURE, TRAIN_SUCCESS, custom::fields::Fields, utils::sql::get_memory};
+use crate::utils::sql::set_conn_db;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -19,7 +28,73 @@ pub fn train(lang: String) -> anyResult<()> {
   let conn = &mut set_conn_db()?;
 
   // train logic
-  train_cli(conn, lang)?;
+  training(conn, lang)?;
+
+  Ok(())
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[rustfmt::skip]
+fn training(conn: &mut SqliteConnection, lang: String) -> anyResult<()> {
+  // retrieve from database
+  let mut cards = get_memory(conn, lang)?;
+
+  // create random number generator
+  let mut rng = rng();
+
+  // shuffle the array
+  cards.shuffle(&mut rng);
+
+  // iterate on data
+  for mut card in cards {
+    print!("\nDo you remember:     {}\nHere is an example:  {}\nPress {} if you remember, {} if you do not, or {} to exit\n",
+      card.item.bright_blue(), card.example.bright_yellow(), "ENTER".bright_white(), "SPACE".bright_white(), "Q".bright_white()
+    );
+
+    // enable raw mode
+    terminal::enable_raw_mode()?;
+
+    if event::poll(std::time::Duration::from_secs(60))? {
+      if let Event::Key(event) = event::read()? {
+        match event.code {
+          KeyCode::Enter => {
+            print!("\n{}\n", TRAIN_SUCCESS[rng.random_range(0..TRAIN_SUCCESS.len())].bright_green());
+            if card.quality.parse::<u32>().context(CoyoteError::Parsing { f: card.quality.clone(), })? < 5 {
+              card.set_field(conn,Fields::Quality,card.quality.parse::<u32>().context(CoyoteError::Parsing { f: card.quality.clone(), })?, 1, |v, f| v + f, )?;
+            }
+          }
+
+          KeyCode::Char(' ') => {
+            print!("\n{}\n", TRAIN_FAILURE[rng.random_range(0..TRAIN_FAILURE.len())].bright_red());
+            if card.quality.parse::<u32>().context(CoyoteError::Parsing { f: card.quality.clone(), })? > 0 {
+              card.set_field(conn,Fields::Quality,card.quality.parse::<u32>().context(CoyoteError::Parsing { f: card.quality.clone(), })?,1,|v, f| v - f, )?;
+            }
+          }
+
+          KeyCode::Char('q') => {
+            println!("Exiting...");
+            break;
+          }
+
+          _ => {}
+        }
+      }
+    }
+
+    // disable raw mode
+    terminal::disable_raw_mode()?;
+
+    // update scores
+    card.update_score(conn)?;
+    print!(
+      "\nQuality: {}, Repetitions: {}, Interval: {}, Ease Factor: {:.2}\n",
+      card.quality, card.repetitions, card.interval, card.difficulty
+    );
+  }
+
+  // disable raw mode
+  terminal::disable_raw_mode()?;
 
   Ok(())
 }
