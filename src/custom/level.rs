cd33f9f75734ca7f30e73d26bd920877e7b46908
Daniel Rivas <danielrivasmd@gmail.com>
2025-02-14 16:07:34 -0600

    derive clone level custom

--- src/custom/level.rs
+++ src/custom/level.rs
@@ -14,7 +14,7 @@ use strum_macros::EnumIter;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[derive(Debug, AsExpression, FromSqlRow, Deserialize, Serialize, EnumIter)]
+#[derive(Debug, Clone, AsExpression, FromSqlRow, Deserialize, Serialize, EnumIter)]
 #[diesel(sql_type = Text)]
 pub enum Level {
   A1,
